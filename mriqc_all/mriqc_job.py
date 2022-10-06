58e37236a0cd56d3fe8951f96ea8a08bee78b9aa
MarsMellow <mpzwiers@gmail.com>
2022-10-06 15:02:08 +0200

    Tweaked logging

--- mriqc_all/mriqc_job.py
+++ mriqc_all/mriqc_job.py
@@ -22,75 +22,82 @@ def main(rawfolder, bidsfolder, bidsmapfile, mriqcfolder, qsiprep):
     running = mriqcfolder.parent/'logs'/f"{rawfolder.parent.name}_{rawfolder.name}.running"
     running.write_text(bidsfolder.as_posix())
 
-    # Make a temporary shadow raw directory structure (with dummy sub-/ses- folders) for unstructered (unscheduled) data
-    if '^' in rawfolder.name:
-        sub = rawfolder.name                    # e.g. JurCla^Prisma_090135.023000
-        ses = rawfolder.name.split('_', 2)[1]   # e.g. 090135.023000
-        rawshadow = Path(tempfile.gettempdir())/'sourcedata'/rawfolder.name
-        subfolder = rawshadow/f"sub-{sub}"
-        subfolder.mkdir(parents=True)
-        (subfolder/f"ses-{ses}").symlink_to(rawfolder)
-        rawfolder = rawshadow
+    try:
 
-    # Convert the rawfolder to a BIDS workfolder
-    print(f">>> Processing {rawfolder}")
-    bidswork = Path(tempfile.gettempdir())/bidsfolder.name
-    bidscoiner.bidscoiner(rawfolder, bidswork, bidsmapfile=bidsmapfile)
-    if not list(bidswork.glob('sub-*')):
-        print(f"No subject data found in: {bidswork}")
-        running.rename(running.with_suffix('.failed'))
-        return
+        # Make a temporary shadow raw directory structure (with dummy sub-/ses- folders) for unstructered (unscheduled) data
+        if '^' in rawfolder.name:
+            sub = rawfolder.name                    # e.g. JurCla^Prisma_090135.023000
+            ses = rawfolder.name.split('_', 2)[1]   # e.g. 090135.023000
+            rawshadow = Path(tempfile.gettempdir())/'sourcedata'/rawfolder.name
+            subfolder = rawshadow/f"sub-{sub}"
+            subfolder.mkdir(parents=True)
+            (subfolder/f"ses-{ses}").symlink_to(rawfolder)
+            rawfolder = rawshadow
 
-    # Run MRIQC participant
-    mriqc_run(bidswork, mriqcfolder, '', nosub=True, skip=False)
+        # Convert the rawfolder to a BIDS workfolder
+        print(f">>> Processing {rawfolder}")
+        bidswork = Path(tempfile.gettempdir())/bidsfolder.name
+        bidscoiner.bidscoiner(rawfolder, bidswork, bidsmapfile=bidsmapfile)
+        if not list(bidswork.glob('sub-*')):
+            print(f"No subject data found in: {bidswork}")
+            running.rename(running.with_suffix('.empty'))
+            return
 
-    # Run QSIPREP (WIP: copy the QC parameters into the MRIQC group report)
-    if qsiprep == 'True':
-        qsiprep_run(bidswork, mriqcfolder, '', resolution='automatic', args='--dwi-only', nthreads=1, nosub=True, skip=False)
+        # Run MRIQC participant
+        mriqc_run(bidswork, mriqcfolder, '', nosub=True, skip=False)
 
-    # Replace all nifti files with dummies
-    for niifile in bidswork.rglob('sub-*.nii*'):
-        niifile.write_text('')
+        # Run QSIPREP (WIP: copy the QC parameters into the MRIQC group report)
+        if qsiprep == 'True':
+            qsiprep_run(bidswork, mriqcfolder, '', resolution='automatic', args='--dwi-only', nthreads=1, nosub=True, skip=False)
 
-    # Update the participants.tsv file in the BIDS sourcefolder if it already exists
-    participantsfile = bidsfolder/'participants.tsv'
-    if participantsfile.is_file():
-        olddata = pd.read_csv(participantsfile, sep='\t', index_col='participant_id')
-        newdata = pd.read_csv(bidswork/'participants.tsv', sep='\t', index_col='participant_id')
-        for participant in newdata.index:
-            if participant not in olddata.index:
-                olddata.loc[participant] = newdata.loc[participant]
-        olddata.to_csv(participantsfile, sep='\t')
+        # Replace all nifti files with dummies
+        for niifile in bidswork.rglob('sub-*.nii*'):
+            niifile.write_text('')
 
-    # Copy the mandatory root files
-    for rootfile in ('README', 'dataset_description.json', 'participants.tsv'):
-        if not (bidsfolder/rootfile).is_file():
-            shutil.copy(bidswork/rootfile, bidsfolder/rootfile)
+        # Update the participants.tsv file in the BIDS sourcefolder if it already exists
+        participantsfile = bidsfolder/'participants.tsv'
+        if participantsfile.is_file():
+            olddata = pd.read_csv(participantsfile, sep='\t', index_col='participant_id')
+            newdata = pd.read_csv(bidswork/'participants.tsv', sep='\t', index_col='participant_id')
+            for participant in newdata.index:
+                if participant not in olddata.index:
+                    olddata.loc[participant] = newdata.loc[participant]
+            olddata.to_csv(participantsfile, sep='\t')
 
-    # Copy the BIDS meta data in each session
-    for subwork in bidswork.glob('sub-*'):
-        subfolder = bidsfolder/subwork.name
-        subfolder.mkdir(parents=True, exist_ok=True)
-        for seswork in sorted(subwork.glob('ses-*')):       # Account for potential previous session in the sub-folder
-            sesfolder = subfolder/seswork.name
-            shutil.copytree(seswork, sesfolder)
+        # Copy the mandatory root files
+        for rootfile in ('README', 'dataset_description.json', 'participants.tsv'):
+            if not (bidsfolder/rootfile).is_file():
+                shutil.copy(bidswork/rootfile, bidsfolder/rootfile)
 
-    # Copy the derived (qsiprep) meta data
-    for subwork in (bidswork/'derivatives'/'qsiprep').glob('sub-*'):
-        subfolder = bidsfolder/'derivatives'/'qsiprep'/subwork.name
-        subfolder.mkdir(parents=True, exist_ok=True)
-        for seswork in sorted(subwork.glob('ses-*')):       # Account for potential previous session in the sub-folder
-            sesfolder = subfolder/seswork.name
-            shutil.copytree(seswork, sesfolder)
+        # Copy the BIDS meta data in each session
+        for subwork in bidswork.glob('sub-*'):
+            subfolder = bidsfolder/subwork.name
+            subfolder.mkdir(parents=True, exist_ok=True)
+            for seswork in sorted(subwork.glob('ses-*')):       # Account for potential previous session in the sub-folder
+                sesfolder = subfolder/seswork.name
+                shutil.copytree(seswork, sesfolder)
 
-    # Write the processed mriqc project folder to the logs
-    if list(mriqcfolder.glob('sub-*.html')):
-        mriqclog = mriqcfolder.parent/'logs'/(mriqcfolder.name + '.meta')
-        mriqclog.write_text('')
-        running.unlink()
-    else:
-        print(f"WARNING: No subject data found in: {mriqcfolder}")
-        running.rename(running.with_suffix('.failed'))
+        # Copy the derived (qsiprep) meta data
+        for subwork in (bidswork/'derivatives'/'qsiprep').glob('sub-*'):
+            subfolder = bidsfolder/'derivatives'/'qsiprep'/subwork.name
+            subfolder.mkdir(parents=True, exist_ok=True)
+            for seswork in sorted(subwork.glob('ses-*')):       # Account for potential previous session in the sub-folder
+                sesfolder = subfolder/seswork.name
+                shutil.copytree(seswork, sesfolder)
+
+        # Write the processed mriqc project folder to the logs
+        if list(mriqcfolder.glob('sub-*.html')):
+            mriqclog = mriqcfolder.parent/'logs'/(mriqcfolder.name + '.meta')
+            mriqclog.write_text('')
+            running.unlink()
+        else:
+            print(f"WARNING: No subject data found in: {mriqcfolder}")
+            running.rename(running.with_suffix('.empty'))
+
+    except Exception as joberror:
+
+        print(f"WARNING: Processing {rawfolder} failed\n{joberror}")
+        running.rename(running.with_suffix('.failed')).write_text(f"Error(s) from processing {bidsfolder}:\n{joberror}")
 
 
 # Shell usage
